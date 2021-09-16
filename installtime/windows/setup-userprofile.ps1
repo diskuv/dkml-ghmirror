216a7dce01d36f3b411f5acc0bc3a593a57e4a60
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-09-16 16:59:08 -0700

    probe

--- installtime/windows/setup-userprofile.ps1
+++ installtime/windows/setup-userprofile.ps1
@@ -1116,6 +1116,9 @@ try {
     # END Compile/install opam.exe
     # ----------------------------------------------------------------
 
+    Invoke-MSYS2CommandWithProgress -MSYS2Dir $MSYS2Dir `
+        -Command "if [[ -e /usr/bin/tree ]]; then tree -F -L 5 '$env:LOCALAPPDATA'; fi"
+
     # ----------------------------------------------------------------
     # BEGIN opam init
 
