ab443b5daa9cc915d755bd28ecba3ada6e5f8df1
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-10-01 13:06:42 -0700

    Install dependencies with retries in CI

--- runtime/unix/build-sandbox-configure.sh
+++ runtime/unix/build-sandbox-configure.sh
@@ -97,13 +97,33 @@ fi
 # BEGIN install code (.opam) dependencies
 
 {
-    echo "'$DKMLDIR'/runtime/unix/platform-opam-exec -b '$BUILDTYPE' -p '$PLATFORM' install --jobs=$NUMCPUS --yes \\"
+    echo "exec '$DKMLDIR'/runtime/unix/platform-opam-exec -b '$BUILDTYPE' -p '$PLATFORM' install --jobs=$NUMCPUS --yes \\"
     if [ "${DKML_BUILD_TRACE:-ON}" = ON ]; then echo "  --debug-level 2 \\"; fi
     echo "  --deps-only --with-test \\"
     printf "  "
     echo "$OPAMS" | sed 's/,/ /g'
 } > "$WORK"/configure.sh
-print_opam_logs_on_error "$DKML_POSIX_SHELL" "$WORK"/configure.sh
+{
+    if [ "${CI:-}" = true ]; then
+        # When we are doing CI, we'll run it twice ... allowing for failures ... and the last time we will run it
+        # without any failures allowed. Also, it is pointless having DKML_BUILD_PRINT_LOGS_ON_ERROR=ON during
+        # retries, except the last one.
+        # shellcheck disable=SC2016
+        echo 'old_bploe="${DKML_BUILD_PRINT_LOGS_ON_ERROR:-}"'
+        echo "export DKML_BUILD_PRINT_LOGS_ON_ERROR=OFF"
+        echo "if $DKML_POSIX_SHELL '$WORK/configure.sh'; then exit 0; fi"
+        echo 'echo Installing code dependencies failed. Retry 1 of 2. >&2'
+        echo "if $DKML_POSIX_SHELL -x '$WORK/configure.sh'; then exit 0; fi"
+        echo 'echo Installing code dependencies failed. Retry 2 of 2. >&2'
+        # shellcheck disable=SC2016
+        echo 'DKML_BUILD_PRINT_LOGS_ON_ERROR="$old_bploe"'
+        echo "exec $DKML_POSIX_SHELL '$WORK/configure.sh'"
+    else
+        echo "exec $DKML_POSIX_SHELL '$WORK/configure.sh'"
+    fi
+} > "$WORK"/configure-with-retry.sh
+
+print_opam_logs_on_error "$DKML_POSIX_SHELL" "$WORK"/configure-with-retry.sh
 
 # END install code (.opam) dependencies
 # -----------------------
