950596bcc46a3485c4a42bcad219f50c9fb2dd91
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-10-04 22:35:10 -0700

    Update ocaml/opam repository version

--- installtime/windows/DeploymentVersion/DeploymentVersion.psm1
+++ installtime/windows/DeploymentVersion/DeploymentVersion.psm1
@@ -7,10 +7,11 @@ Param()
 $DV_AvailableOpamVersion = "2.1.0.msys2.9" # needs to be a real Opam tag in https://github.com/diskuv/opam!
 Export-ModuleMember -Variable DV_AvailableOpamVersion
 
-# https://hub.docker.com/layers/ocaml/opam/windows-msvc-20H2-ocaml-4.12/images/sha256-e7b6e08cf22f6caed6599f801fbafbc32a93545e864b83ab42aedbd0d5835b55?context=explore
+# https://hub.docker.com/r/ocaml/opam/tags?page=1&ordering=last_updated&name=windows-msvc-20H2-ocaml-4.12
 # Q: Why 20H2? Ans:
 #    1. because it is a single kernel image so it is much smaller than multikernel `windows-msvc`
 #    2. it is the latest as of 2021-08-05 so it will be a long time before that Windows kernel is no longer built;
 #       would be nice if we could query https://github.com/avsm/ocaml-dockerfile/blob/ac54d3550159b0450032f0f6a996c2e96d3cafd7/src-opam/dockerfile_distro.ml#L36-L47
-$DV_WindowsMsvcDockerImage = "ocaml/opam:windows-msvc-20H2-ocaml-4.12@sha256:e7b6e08cf22f6caed6599f801fbafbc32a93545e864b83ab42aedbd0d5835b55"
+# Note: You must update this once every couple months because Docker Hub removes old versions.
+$DV_WindowsMsvcDockerImage = "ocaml/opam:windows-msvc-20H2-ocaml-4.12@sha256:810ce2fca08c22ea1bf4066cb75ffcba2f98142d6ce09162905d9ddc09967da8"
 Export-ModuleMember -Variable DV_WindowsMsvcDockerImage
