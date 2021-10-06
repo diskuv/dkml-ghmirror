ab21b17c1429e89220e87c6fe0efca5d016167ad
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2021-10-06 09:17:16 -0700

    Revert "Debug why WORK is being removed"
    This reverts commit 645b267e4c13db19bcdb84799d6a12c702aa0173.

--- runtime/unix/_common_tool.sh
+++ runtime/unix/_common_tool.sh
@@ -90,12 +90,7 @@ TMPPARENTDIR_RELTOP="build/_tmp"
 TMPPARENTDIR_BUILDHOST="${TMPPARENTDIR_BUILDHOST:-$TOPDIR/$TMPPARENTDIR_RELTOP}"
 install -d "$TMPPARENTDIR_BUILDHOST"
 WORK=$(mktemp -d "$TMPPARENTDIR_BUILDHOST"/dkmlw.XXXXX)
-echo "IMPORTANT: Created WORK - $WORK" >&2 # TODO REMOVE
-common_tool_cleanup() {
-    echo "Removing $WORK dir from trapped EXIT signal" >&2
-    rm -rf "$WORK"
-}
-trap 'common_tool_cleanup' EXIT # TODO REVERT
+trap 'rm -rf "$WORK"' EXIT
 WORK_BASENAME=$(basename "$WORK")
 # shellcheck disable=SC2034
 WORK_EXPAND="@@EXPAND_TOPDIR@@/$TMPPARENTDIR_RELTOP/$WORK_BASENAME"
