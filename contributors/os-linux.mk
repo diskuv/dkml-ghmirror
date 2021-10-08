4e27c38e18360ea0e1731544fa06660fd78421a9
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-10-07 23:50:05 -0700

    Contributors: Linux builds of HTML pages

---
+++ contributors/os-linux.mk
@@ -0,0 +1,11 @@
+install-gitlab-cli:
+	echo "Currently Linux can only be used for creating HTML pages. TODO: Install gitlab CLI"
+
+install-powershell: # for some reason ls --versions does not work
+	echo "Currently Linux can only be used for creating HTML pages. TODO: Install Powershell"
+
+install-shellcheck:
+	echo "Currently Linux can only be used for creating HTML pages. TODO: Install shellcheck"
+
+install-gitlab-release-cli:
+	echo "Currently Linux can only be used for creating HTML pages. TODO: Install gitlab release CLI"
