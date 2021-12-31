b0298c7066c40868697aeb6a0562fc8b7e1b044f
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2021-12-31 00:58:36 -0800

    Unset CC/AS/etc will use ./configure autodetect

--- installtime/unix/private/reproducible-compile-ocaml-example_1.sh
+++ installtime/unix/private/reproducible-compile-ocaml-example_1.sh
@@ -49,60 +49,52 @@ export CFLAGS
 # CC
 # The value of this appears in `ocamlc -config`; will be viral to most Opam packages with embedded C code
 # clang and perhaps other compilers need --target=armv7-none-linux-androideabi21 for example
-CC="$CC ${DKML_COMPILE_CM_CMAKE_C_COMPILE_OPTIONS_TARGET:-}${DKML_COMPILE_CM_CMAKE_C_COMPILER_TARGET:-}"
-# clang and perhaps other compilers need --sysroot=C:/Users/beckf/AppData/Local/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/sysroot for example
-CC="$CC ${DKML_COMPILE_CM_CMAKE_C_COMPILE_OPTIONS_SYSROOT:-}${DKML_COMPILE_CM_CMAKE_SYSROOT:-}"
+if [ -n "${CC:-}" ]; then
+  CC="$CC ${DKML_COMPILE_CM_CMAKE_C_COMPILE_OPTIONS_TARGET:-}${DKML_COMPILE_CM_CMAKE_C_COMPILER_TARGET:-}"
+  # clang and perhaps other compilers need --sysroot=C:/Users/beckf/AppData/Local/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/sysroot for example
+  CC="$CC ${DKML_COMPILE_CM_CMAKE_C_COMPILE_OPTIONS_SYSROOT:-}${DKML_COMPILE_CM_CMAKE_SYSROOT:-}"
+fi
 export CC
 
-## DEP_CC
-## In ./configure this will be used to capture dependencies with -MT and -MT flags. Unlike CC, no
-## CFLAGS are used with it which means important flags like -fPIC are lost in the dependencies.
-## That makes linking impossible for clang (at least for Android armeabi-v7a). clang and gcc are
-## capable of capturing dependencies, so just use the CC + CFLAGS compiler
-#case "${DKML_COMPILE_CM_CMAKE_C_COMPILER_ID:-}" in
-#  AppleClang|Clang|GNU)
-#    DEP_CC="$CC $CFLAGS"
-#    export DEP_CC
-#    ;;
-#esac
-
 # https://github.com/ocaml/ocaml/blob/01c6f16cc69ce1d8cf157e66d5702fadaa18d247/configure.ac#L1213-L1240
-_TMP_AS="${AS:-}"
-AS="$_TMP_AS ${DKML_COMPILE_CM_CMAKE_ASM_COMPILE_OPTIONS_TARGET:-}${DKML_COMPILE_CM_CMAKE_ASM_COMPILER_TARGET:-} $ASFLAGS"
-case "$DKML_TARGET_ABI" in
-  darwin_*) AS="$AS -Wno-trigraphs" ;;
-esac
-if printf "%s" "${DKML_COMPILE_CM_CMAKE_ASM_COMPILE_OBJECT:-}" | PATH=/usr/bin:/bin grep -q -E -- '\B-c\b'; then
-  # CMAKE_ASM_COMPILE_OBJECT contains '-c' as a single word. Ex: <CMAKE_ASM_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -o <OBJECT> -c <SOURCE>
-  AS="$AS -c"
-fi
-ASPP="$AS"
-case "${DKML_COMPILE_CM_CMAKE_ASM_COMPILER_ID:-}" in
-  AppleClang|Clang)
-    # For clang, AS should be `as` so that relocations work properly and ASPP should be `clang -c`.
-    # So we try <bindir>/<CMAKE_ANDROID_ARCH_TRIPLE>-as and <bindir>/<CMAKE_LIBRARY_ARCHITECTURE>-as.
-    # (CMAKE_LIBRARY_ARCHITECTURE is poorly documented, but https://lldb.llvm.org/resources/build.html
-    # indicates it is typically set to the architecture triple; Android NDK does this).
-    _asm_compiler_bindir=$(PATH=/usr/bin:/bin dirname "$DKML_COMPILE_CM_CMAKE_ASM_COMPILER")
-    _asm_compiler_as=
-    if [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as.exe" ]; then
-      _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as.exe"
-    elif [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as" ]; then
-      _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as"
+if [ -n "${AS:-}" ]; then
+  _TMP_AS="${AS:-}"
+  AS="$_TMP_AS ${DKML_COMPILE_CM_CMAKE_ASM_COMPILE_OPTIONS_TARGET:-}${DKML_COMPILE_CM_CMAKE_ASM_COMPILER_TARGET:-} $ASFLAGS"
+  case "$DKML_TARGET_ABI" in
+    darwin_*) AS="$AS -Wno-trigraphs" ;;
+  esac
+  if printf "%s" "${DKML_COMPILE_CM_CMAKE_ASM_COMPILE_OBJECT:-}" | PATH=/usr/bin:/bin grep -q -E -- '\B-c\b'; then
+    # CMAKE_ASM_COMPILE_OBJECT contains '-c' as a single word. Ex: <CMAKE_ASM_COMPILER> <DEFINES> <INCLUDES> <FLAGS> -o <OBJECT> -c <SOURCE>
+    AS="$AS -c"
+  fi
+  ASPP="$AS"
+  case "${DKML_COMPILE_CM_CMAKE_ASM_COMPILER_ID:-}" in
+    AppleClang|Clang)
+      # For clang, AS should be `as` so that relocations work properly and ASPP should be `clang -c`.
+      # So we try <bindir>/<CMAKE_ANDROID_ARCH_TRIPLE>-as and <bindir>/<CMAKE_LIBRARY_ARCHITECTURE>-as.
+      # (CMAKE_LIBRARY_ARCHITECTURE is poorly documented, but https://lldb.llvm.org/resources/build.html
+      # indicates it is typically set to the architecture triple; Android NDK does this).
+      _asm_compiler_bindir=$(PATH=/usr/bin:/bin dirname "$DKML_COMPILE_CM_CMAKE_ASM_COMPILER")
+      _asm_compiler_as=
+      if [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as.exe" ]; then
+        _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as.exe"
+      elif [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as" ]; then
+        _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_ANDROID_ARCH_TRIPLE:-}-as"
 
-    elif [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as.exe" ]; then
-      _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as.exe"
-    elif [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as" ]; then
-      _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as"
-    fi
-    if [ -n "$_asm_compiler_as" ]; then
-      if [ -x /usr/bin/cygpath ]; then
-        _asm_compiler_as=$(/usr/bin/cygpath -am "$_asm_compiler_as")
+      elif [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as.exe" ]; then
+        _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as.exe"
+      elif [ -e "$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as" ]; then
+        _asm_compiler_as="$_asm_compiler_bindir/${DKML_COMPILE_CM_CMAKE_LIBRARY_ARCHITECTURE:-}-as"
       fi
-      AS="$_asm_compiler_as"
-    fi
-    ;;
-esac
+      if [ -n "$_asm_compiler_as" ]; then
+        if [ -x /usr/bin/cygpath ]; then
+          _asm_compiler_as=$(/usr/bin/cygpath -am "$_asm_compiler_as")
+        fi
+        AS="$_asm_compiler_as"
+      fi
+      ;;
+  esac
+fi
 export AS ASPP
 
 # https://github.com/ocaml/ocaml/blob/01c6f16cc69ce1d8cf157e66d5702fadaa18d247/configure#L3434-L3534
