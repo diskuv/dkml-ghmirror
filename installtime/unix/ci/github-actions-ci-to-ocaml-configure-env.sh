5baed92a7043116d4d4d4c0039efdf7f4ef6da5b
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-01-15 18:27:28 -0800

    Import NDK settings

--- installtime/unix/ci/github-actions-ci-to-ocaml-configure-env.sh
+++ installtime/unix/ci/github-actions-ci-to-ocaml-configure-env.sh
@@ -68,12 +68,13 @@ TOOLCHAIN=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/$HOST_TAG
 
 #   Triple
 case "$DKML_TARGET_ABI" in
-    # TARGET = 'Triple' in https://developer.android.com/ndk/guides/other_build_systems#overview
-    # LLVM_TRIPLE = https://llvm.org/doxygen/classllvm_1_1Triple.html
-    android_arm64v8a) TARGET=aarch64-linux-android ;    LLVM_TRIPLE=aarch64-linux-android ;;
-    android_arm32v7a) TARGET=armv7a-linux-androideabi ; LLVM_TRIPLE=arm-linux-androideabi ;;
-    android_x86)      TARGET=i686-linux-android ;       LLVM_TRIPLE=i686-linux-android ;;
-    android_x86_64)   TARGET=x86_64-linux-android ;     LLVM_TRIPLE=x86_64-linux-android ;;
+    # TOOLCHAIN_NAME_CLANG 'Triple' in https://developer.android.com/ndk/guides/other_build_systems#overview
+    # TOOLCHAIN_NAME_AS     https://chromium.googlesource.com/android_ndk/+/401019bf85744311b26c88ced255cd53401af8b7/build/core/toolchains/aarch64-linux-android-clang/setup.mk#16
+    # LLVM_TRIPLE           https://chromium.googlesource.com/android_ndk/+/401019bf85744311b26c88ced255cd53401af8b7/build/core/toolchains/aarch64-linux-android-clang/setup.mk#17
+    android_arm64v8a) TOOLCHAIN_NAME_CLANG=aarch64-linux-android ;    TOOLCHAIN_NAME_AS=aarch64-linux-android ; LLVM_TRIPLE=aarch64-none-linux-android ;;
+    android_arm32v7a) TOOLCHAIN_NAME_CLANG=armv7a-linux-androideabi ; TOOLCHAIN_NAME_AS=arm-linux-androideabi ; LLVM_TRIPLE=armv7-none-linux-androideabi ;;
+    android_x86)      TOOLCHAIN_NAME_CLANG=i686-linux-android ;       TOOLCHAIN_NAME_AS=i686-linux-android ;    LLVM_TRIPLE=i686-none-linux-android;;
+    android_x86_64)   TOOLCHAIN_NAME_CLANG=x86_64-linux-android ;     TOOLCHAIN_NAME_AS=x86_64-linux-android ;  LLVM_TRIPLE=x86_64-none-linux-android ;;
     *)
         printf "FATAL: The DKML_TARGET_ABI must be an DKML Android ABI, not %s\n" "$DKML_TARGET_ABI" >&2
         exit 107
@@ -84,15 +85,18 @@ esac
 #       https://developer.android.com/ndk/guides/other_build_systems#autoconf
 find "$TOOLCHAIN/bin" -type f -name '*-clang' # Show API versions for debugging
 find "$TOOLCHAIN/bin" -type f -name '*-as'    # More debugging
+#       Dump of Android NDK r23 flags. And -g3 and -g for debugging.
+_android_cflags="-fPIE -fPIC -DANDROID -fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -D_FORTIFY_SOURCE=2 -Wformat -Werror=format-security -fexceptions -g3 -g  -fno-limit-debug-info"
 export AR="$TOOLCHAIN/bin/llvm-ar"
-export CC="$TOOLCHAIN/bin/$TARGET$API-clang"
+export CC="$TOOLCHAIN/bin/$TOOLCHAIN_NAME_CLANG$API-clang"
+! [ -x "$CC" ] && printf "FATAL: No clang compiler at %s\n" "$CC" >&2 && exit 107
 export LD="$TOOLCHAIN/bin/ld"
 export DIRECT_LD="$LD"
 export RANLIB="$TOOLCHAIN/bin/llvm-ranlib"
 export STRIP="$TOOLCHAIN/bin/llvm-strip"
 export NM="$TOOLCHAIN/bin/llvm-nm"
 export OBJDUMP="$TOOLCHAIN/bin/llvm-objdump"
-export CFLAGS=
+export CFLAGS=$_android_cflags
 export LDFLAGS=
 #       Android NDK comes with a) a Clang compiler and b) a GNU AS assembler and c) sometimes a YASM assembler
 #       in its bin folder
@@ -100,5 +104,6 @@ export LDFLAGS=
 #
 #       The GNU AS assembler (https://sourceware.org/binutils/docs/as/index.html) does not support preprocessing
 #       so it cannot be used as the `ASPP` ./configure variable.
-export AS="$TOOLCHAIN/bin/$LLVM_TRIPLE-as"
-export ASPP="$TOOLCHAIN/bin/clang -target $TARGET$API -c"
+export AS="$TOOLCHAIN/bin/$TOOLCHAIN_NAME_AS-as"
+! [ -x "$AS" ] && printf "FATAL: No assembler at %s\n" "$AS" >&2 && exit 107
+export ASPP="$TOOLCHAIN/bin/clang --target=$LLVM_TRIPLE$API $_android_cflags -c"
