aa1a4b54aa3d9913e4743761c1db03b13fa8f129
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-02-02 17:40:30 -0800

    Add vendor dir to distribution archive

--- contributors/release.sh
+++ contributors/release.sh
@@ -159,7 +159,7 @@ fi
 
 # Define which files and directories go into distribution archive
 # - The *.opam should be in sync with (P1) in installtime/windows/setup-userprofile.ps1
-ARCHIVE_MEMBERS=(LICENSE.txt README.md opam-dkml.opam dkml-apps.opam etc buildtime installtime runtime .dkmlroot .gitattributes .gitignore)
+ARCHIVE_MEMBERS=(LICENSE.txt README.md opam-dkml.opam dkml-apps.opam etc buildtime installtime runtime vendor .dkmlroot .gitattributes .gitignore)
 
 # Make _build/distribution-portable.zip
 FILE="$DKMLDIR/contributors/_build/distribution-portable.zip"
