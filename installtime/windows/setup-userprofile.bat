35253d89b6a31a627c3f8e31764a700c3b572f62
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-04-01 02:16:46 -0700

    Move PowerShell scripts into ocamlcompiler

--- installtime/windows/setup-userprofile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@REM Microsoft way of getting around PowerShell permissions: https://github.com/microsoft/vcpkg/blob/71422c627264daedcbcd46f01f1ed0dcd8460f1b/bootstrap-vcpkg.bat
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {& '%~dp0setup-userprofile.ps1' %*; exit $LASTEXITCODE}"
-exit /b %ERRORLEVEL%
