442eaa46f535081ba1d429c1a30a391674d949e3
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-05-09 07:21:17 -0700

    Add non-vendored GitHub to release

--- contributors/release.sh
+++ contributors/release.sh
@@ -8,8 +8,23 @@ DKMLDIR=$(dirname "$0")
 DKMLDIR=$(cd "$DKMLDIR/.." && pwd)
 
 # Which vendor/<dir> should be version synced with this
-SYNCED_PRERELEASE_VENDORS=(drc drd diskuv-opam-repository)
+SYNCED_PRERELEASE_BEFORE_APPS=(drc drd)
+SYNCED_PRERELEASE_AFTER_APPS=(diskuv-opam-repository)
+SYNCED_PRERELEASE_VENDORS=("${SYNCED_PRERELEASE_BEFORE_APPS[@]}" "${SYNCED_PRERELEASE_AFTER_APPS[@]}")
 SYNCED_RELEASE_VENDORS=(diskuv-opam-repository)
+# Which non-vendored Git projects should be synced
+GITURLS=(
+    https://github.com/diskuv/dkml-runtime-apps.git
+    https://github.com/diskuv/dkml-component-opam.git
+    https://github.com/diskuv/dkml-component-ocamlrun.git
+    https://github.com/diskuv/dkml-component-ocamlcompiler.git
+)
+COMPONENTPATHS=(
+    dkml-component-ocamlcompiler/dkml-component-network-ocamlcompiler.opam
+    dkml-component-ocamlrun/dkml-component-staging-ocamlrun.opam
+    dkml-component-opam/dkml-component-staging-opam32.opam
+    dkml-component-opam/dkml-component-staging-opam64.opam
+)
 
 # ------------------
 # BEGIN Command line processing
@@ -60,7 +75,94 @@ export GITLAB_PRIVATE_TOKEN=$($GLAB auth status -t 2>&1 | awk '$2=="Token:" {pri
 # Git, especially through bump2version, needs HOME set for Windows
 if which pacman >/dev/null 2>&1 && which cygpath >/dev/null 2>&1; then HOME="$USERPROFILE"; fi
 
+# Source dirs of git clones
+SRC="$DKMLDIR/_build/src"
+install -d "$SRC"
+
+# Work directory
+WORK=$(PATH=/usr/bin:/bin mktemp -d "$DKMLDIR"/_build/release.XXXXX)
+trap 'PATH=/usr/bin:/bin rm -rf "$WORK"' EXIT
+
 cd "$DKMLDIR"
+
+# Load cross platform functions
+#   shellcheck disable=SC1091
+. "vendor/drc/unix/crossplatform-functions.sh"
+
+# ------------------------
+# Commits and tags
+# ------------------------
+
+gitdir() {
+    gitdir_URL=$1
+    gitdir_DIR=$(basename "$gitdir_URL")
+    printf "%s" "$gitdir_DIR" | sed 's#.git$##'
+}
+opam_source_block() {
+    opam_source_block_TYPE=$1
+    shift
+    opam_source_block_VER=$1
+    shift
+    opam_source_block_PKG=$1
+    shift
+    opam_source_block_FILE=$1
+    shift
+
+    log_trace "$DKMLSYS_CURL" -L "https://github.com/diskuv/$opam_source_block_PKG/archive/refs/tags/$opam_source_block_VER.tar.gz" -o "$WORK/a.tar.gz"
+    log_trace tar tfz "$WORK/a.tar.gz" # make sure we have a valid download
+    opam_source_block_CKSUM=$(sha256compute "$WORK/a.tar.gz")
+
+    {
+        if [ "$opam_source_block_TYPE" = url ]; then
+            printf "url {\n"
+        else
+            printf "%s \"%s\" {\n" "$opam_source_block_TYPE" "dl/$opam_source_block_PKG.tar.gz"
+        fi
+        printf "  src: \"%s\"\n" "https://github.com/diskuv/$opam_source_block_PKG/archive/refs/tags/$opam_source_block_VER.tar.gz"
+        printf "%s\n" '  checksum: ['
+        printf "    \"sha256=%s\"\n" "$opam_source_block_CKSUM"
+        printf "%s\n" '  ]'
+        printf "%s\n" '}'
+    } > "$opam_source_block_FILE"
+}
+remove_opam_extra_source() {
+    remove_opam_extra_source_PKG=$1
+    shift
+    awk '/^extra-source "dl\/'"$remove_opam_extra_source_PKG"'.tar.gz"/ {got=1}   !got {print}   got==1 && /^}$/ {got=0}'
+}
+remove_opam_url_source() {
+    awk '/^url {/ {got=1}   !got {print}   got==1 && /^}$/ {got=0}'
+}
+update_drc_drd() {
+    update_drc_drd_FILE=$1
+    shift
+    # shellcheck disable=SC2002
+    cat "$update_drc_drd_FILE" | \
+        remove_opam_extra_source dkml-runtime-common | \
+        remove_opam_extra_source dkml-runtime-distribution | \
+        cat - "$WORK/dkml-runtime-common.extra-source" "$WORK/dkml-runtime-distribution.extra-source" > \
+        "$update_drc_drd_FILE".tmp
+    mv "$update_drc_drd_FILE".tmp "$update_drc_drd_FILE"
+}
+rungit() {
+    if [ -n "${COMSPEC:-}" ]; then
+        HOME="$USERPROFILE" git "$@"
+    else
+        git "$@"
+    fi
+}
+
+# Checkout or update non-vendored Git URLs
+for GITURL in "${GITURLS[@]}"; do
+    GITDIR=$SRC/$(gitdir "$GITURL")
+    if [ -d "$GITDIR" ]; then
+        git -C "$GITDIR" clean -d -x -f
+        git -C "$GITDIR" reset --hard origin/main
+        git -C "$GITDIR" pull --ff-only
+    else
+        git -C "$SRC" clone "$GITURL"
+    fi
+done
 
 # Capture which version will be the release version when the prereleases are finished
 TARGET_VERSION=$(awk '$1=="current_version"{print $NF; exit 0}' .bumpversion.prerelease.cfg | sed 's/[-+].*//')
@@ -70,6 +172,20 @@ get_new_version() {
 get_new_version
 CURRENT_VERSION=$NEW_VERSION
 
+OPAM_CURRENT_VERSION=$(printf "%s" "$CURRENT_VERSION" | tr -d v | tr - '~')
+OPAM_NEW_VERSION=$(printf "%s" "$NEW_VERSION" | tr -d v | tr - '~')
+
+DKMLAPPS_OLDOPAM="vendor/diskuv-opam-repository/packages/dkml-apps/dkml-apps.$OPAM_CURRENT_VERSION/opam"
+DKMLAPPS_NEWOPAM="vendor/diskuv-opam-repository/packages/dkml-apps/dkml-apps.$OPAM_NEW_VERSION/opam"
+
+#   validate
+if [ ! -e "$DKMLAPPS_OLDOPAM" ]; then
+    printf "FATAL: Could not find %s\n" "$DKMLAPPS_OLDOPAM" >&2
+    exit 1
+fi
+
+# Do release commits
+autodetect_system_binaries # find DKMLSYS_CURL
 if [ "$PRERELEASE" = ON ]; then
     # Increment the prerelease
 
@@ -89,12 +205,6 @@ if [ "$PRERELEASE" = ON ]; then
         git -C vendor/"$v" commit -m "Bump version: $CURRENT_VERSION → $NEW_VERSION"
     done
 	git commit -m "Bump version: $CURRENT_VERSION → $NEW_VERSION" -a
-
-    # 3. Tag
-    for v in "${SYNCED_PRERELEASE_VENDORS[@]}"; do
-        git -C vendor/"$v" tag "v$NEW_VERSION"
-    done
-	git tag "v$NEW_VERSION"
 else
     # We are doing a target release, not a prerelease ...
 
@@ -133,10 +243,8 @@ else
         --verbose
     for v in "${SYNCED_PRERELEASE_VENDORS[@]}"; do
         git -C vendor/"$v" commit -m "Finish v$TARGET_VERSION release (2 of 2)" -a
-        git -C vendor/"$v" tag "v$TARGET_VERSION"
     done
     git commit -m "Finish v$TARGET_VERSION release (2 of 2)" -a
-    git tag "v$TARGET_VERSION"
 fi
 
 # Safety check version for a release
@@ -146,8 +254,62 @@ if [ "$PRERELEASE" = OFF ]; then
         echo "The target version $TARGET_VERSION and the new version $NEW_VERSION did not match" >&2
         exit 1
     fi
-    NEW_VERSION="$TARGET_VERSION"
+    OUT_VERSION=$TARGET_VERSION
+else
+    OUT_VERSION=$NEW_VERSION
 fi
+
+# Tag and push before dkml-runtime-apps
+for v in "${SYNCED_PRERELEASE_BEFORE_APPS[@]}"; do
+    git -C vendor/"$v" tag "v$OUT_VERSION"
+    git -C vendor/"$v" push --atomic origin main "v$OUT_VERSION"
+done
+
+# Update, tag and push dkml-runtime-apps (and components and opam-repository)
+#   Calculate new extra-source blocks; wait 5 seconds to make sure
+#   dkml-runtime-common|distribution GitHub tarballs are eventually consistent
+sleep 5
+opam_source_block extra-source "v$OUT_VERSION" dkml-runtime-common       "$WORK/dkml-runtime-common.extra-source"
+opam_source_block extra-source "v$OUT_VERSION" dkml-runtime-distribution "$WORK/dkml-runtime-distribution.extra-source"
+#   Update and push dkml-runtime-apps which is used by diskuv-opam-repository
+update_drc_drd "$SRC/dkml-runtime-apps/dkml-runtime.opam"
+update_drc_drd "$SRC/dkml-runtime-apps/dkml-runtime.opam.template"
+rungit -C "$SRC/dkml-runtime-apps" commit -a -m "Bump version: $CURRENT_VERSION → $OUT_VERSION"
+rungit -C "$SRC/dkml-runtime-apps" tag "v$OUT_VERSION"
+rungit -C "$SRC/dkml-runtime-apps" push --atomic origin main "v$OUT_VERSION"
+#   Update and push components
+for COMPONENTPATH in "${COMPONENTPATHS[@]}"; do
+    COMPONENTDIR=$(dirname "$SRC/$COMPONENTPATH")
+    update_drc_drd "$SRC/$COMPONENTPATH"
+    rungit -C "$COMPONENTDIR" commit -a -m "dkml-runtime-{common,distribution} $OUT_VERSION"
+    rungit -C "$COMPONENTDIR" push origin main
+done
+#   Calculate new extra-source blocks; wait 5 seconds to make sure
+#   dkml-runtime-apps GitHub tarball is eventually consistent
+sleep 5
+opam_source_block url "v$OUT_VERSION" dkml-runtime-apps "$WORK/dkml-runtime-apps.url"
+#   Update diskuv-opam-repository
+DKMLAPPS_NEWDIR=$(dirname "$DKMLAPPS_NEWOPAM")
+install -d "$DKMLAPPS_NEWDIR"
+#       shellcheck disable=SC2002
+cat "$DKMLAPPS_OLDOPAM" | \
+    remove_opam_url_source | \
+    cat - "$WORK/dkml-runtime-apps.url" > \
+    "$DKMLAPPS_NEWOPAM".tmp
+mv "$DKMLAPPS_NEWOPAM".tmp "$DKMLAPPS_NEWOPAM"
+rungit -C "vendor/diskuv-opam-repository" add "$DKMLAPPS_NEWOPAM"
+rungit -C "vendor/diskuv-opam-repository" commit -m "dkml-runtime-apps $OUT_VERSION"
+
+# Tag and push after dkml-runtime-apps
+for v in "${SYNCED_PRERELEASE_AFTER_APPS[@]}"; do
+    git -C vendor/"$v" tag "v$OUT_VERSION"
+    git -C vendor/"$v" push --atomic origin main "v$OUT_VERSION"
+done
+git push --atomic origin main "v$OUT_VERSION"
+
+# ------------------------
+# Distribution archive
+# ------------------------
 
 # Define which files and directories go into distribution archive
 ARCHIVE_MEMBERS=(LICENSE.txt README.md etc buildtime installtime runtime vendor .dkmlroot .gitattributes .gitignore)
@@ -181,32 +343,22 @@ else
     tar cvfz "$FILE" -s ',^,diskuv-ocaml/,' --uname root --gname root --exclude _build --no-xattrs "${ARCHIVE_MEMBERS[@]}"
 fi
 
-# Push
-for v in "${SYNCED_PRERELEASE_VENDORS[@]}"; do
-    git -C vendor/"$v" push
-    git -C vendor/"$v" push --tags
-done
-git push
-git push --tags
-# `git push --atomic origin main "v$NEW_VERSION"` is similar but we don't have to hardcode the branch, and GitLab
-# can trigger both its CI_COMMIT_TAG rules and its CI_COMMIT_BRANCH rules.
-
 # Set GitLab options
 CI_SERVER_URL=https://gitlab.com
 CI_API_V4_URL="$CI_SERVER_URL/api/v4"
 CI_PROJECT_ID='diskuv%2Fdiskuv-ocaml' # Must be url-encoded per https://docs.gitlab.com/ee/user/packages/generic_packages/
 GLOBAL_OPTS=(--server-url "$CI_SERVER_URL" --project-id "$CI_PROJECT_ID")
 CREATE_OPTS=(
-    --tag-name "v$NEW_VERSION"
+    --tag-name "v$OUT_VERSION"
 )
 if [ "$PRERELEASE" = OFF ]; then
     CREATE_OPTS+=(
-        --name "Version $NEW_VERSION"
-        --description "contributors/changes/v$NEW_VERSION.md"
+        --name "Version $OUT_VERSION"
+        --description "contributors/changes/v$OUT_VERSION.md"
     )
 else
     CREATE_OPTS+=(
-        --name "$NEW_VERSION (alpha prerelease of $TARGET_VERSION)"
+        --name "$OUT_VERSION (alpha prerelease of $TARGET_VERSION)"
     )
 fi
 if [ -e /usr/ssl/cert.pem ]; then
@@ -219,10 +371,14 @@ if false; then
     CREATE_OPTS+=(--milestone "v$TARGET_VERSION")
 fi
 
+# ------------------------
+# GitLab Release
+# ------------------------
+
 # Setup Generic Packages (https://docs.gitlab.com/ee/user/packages/generic_packages/)
 PACKAGE_REGISTRY_GENERIC_URL="$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/generic"
-DISTPORTABLE_NEWURL="$PACKAGE_REGISTRY_GENERIC_URL/distribution-portable/$NEW_VERSION"
-OOREPO_NEWURL="$PACKAGE_REGISTRY_GENERIC_URL/ocaml_opam_repo-reproducible/$NEW_VERSION"
+DISTPORTABLE_NEWURL="$PACKAGE_REGISTRY_GENERIC_URL/distribution-portable/$OUT_VERSION"
+OOREPO_NEWURL="$PACKAGE_REGISTRY_GENERIC_URL/ocaml_opam_repo-reproducible/$OUT_VERSION"
 OOREPO_OLDURL="$PACKAGE_REGISTRY_GENERIC_URL/ocaml_opam_repo-reproducible/$CURRENT_VERSION"
 OCAMLVERS=(4.13.1 4.12.1)
 #GITLAB_TARGET_VERSION=$(printf "%s" "$TARGET_VERSION" | tr +- ..) # replace -prerelM and +commitN with .prerelM and .commitN
@@ -256,12 +412,12 @@ CREATE_OPTS+=(--assets-link "{\"name\":\"Diskuv OCaml distribution (tar.gz) [por
 
 # Reference the Generic Packages that GitLab automatically creates
 for OCAMLVER in "${OCAMLVERS[@]}"; do
-    CREATE_OPTS+=(--assets-link "{\"name\":\"Vanilla OCaml $OCAMLVER for 64-bit Windows (zip) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml-reproducible/v$NEW_VERSION/ocaml-$OCAMLVER-windows_x86_64.zip\"}")
-    CREATE_OPTS+=(--assets-link "{\"name\":\"Vanilla OCaml $OCAMLVER for 32-bit Windows (zip) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml-reproducible/v$NEW_VERSION/ocaml-$OCAMLVER-windows_x86.zip\"}")
+    CREATE_OPTS+=(--assets-link "{\"name\":\"Vanilla OCaml $OCAMLVER for 64-bit Windows (zip) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml-reproducible/$OUT_VERSION/ocaml-$OCAMLVER-windows_x86_64.zip\"}")
+    CREATE_OPTS+=(--assets-link "{\"name\":\"Vanilla OCaml $OCAMLVER for 32-bit Windows (zip) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml-reproducible/$OUT_VERSION/ocaml-$OCAMLVER-windows_x86.zip\"}")
 done
 for OCAMLVER in "${OCAMLVERS[@]}"; do
-    CREATE_OPTS+=(--assets-link "{\"name\":\"OCaml $OCAMLVER tailored Opam repository (tar.gz) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml_opam_repo-reproducible/v$NEW_VERSION/ocaml-opam-repo-$OCAMLVER.tar.gz\"}")
-    CREATE_OPTS+=(--assets-link "{\"name\":\"OCaml $OCAMLVER tailored Opam repository (zip) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml_opam_repo-reproducible/v$NEW_VERSION/ocaml-opam-repo-$OCAMLVER.zip\"}")
+    CREATE_OPTS+=(--assets-link "{\"name\":\"OCaml $OCAMLVER tailored Opam repository (tar.gz) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml_opam_repo-reproducible/$OUT_VERSION/ocaml-opam-repo-$OCAMLVER.tar.gz\"}")
+    CREATE_OPTS+=(--assets-link "{\"name\":\"OCaml $OCAMLVER tailored Opam repository (zip) [reproducible;Apache-2.0]\",\"url\":\"${PACKAGE_REGISTRY_GENERIC_URL}/ocaml_opam_repo-reproducible/$OUT_VERSION/ocaml-opam-repo-$OCAMLVER.zip\"}")
 done
 
 # Create the release
