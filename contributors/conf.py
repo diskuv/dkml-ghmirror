74ec5b762ee68d36647d41022fb79178d9f47426
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-14 23:06:54 -0800

    Bump version: 1.0.2-prerel31 → 1.0.2-prerel32

--- contributors/conf.py
+++ contributors/conf.py
@@ -21,7 +21,7 @@ import subprocess
 
 # -- Project information -----------------------------------------------------
 
-project = 'Diskuv OCaml 1.0.2-prerel31'
+project = 'Diskuv OCaml 1.0.2-prerel32'
 copyright = '2021, Diskuv, Inc.'
 author = 'Diskuv, Inc.'
 
