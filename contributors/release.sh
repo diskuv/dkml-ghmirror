c1f42df30db68045b819b328cce481f9c3bfa0e7
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-18 23:14:34 -0800

    Clean before building ocaml_opam_repo_trim.bc
    * Without a clean an old version from an old compiler of the
       .bc may be uploaded, leading to bytecode magic number errors

--- contributors/release.sh
+++ contributors/release.sh
@@ -597,6 +597,7 @@ if [ "$QUICK" = ON ]; then
 fi
 
 # Build ocaml_opam_repo_trim.bc (after the vendor/drd/ is cleaned, and just before upload)
+opam exec -- dune clean --root vendor/drd/src/ml
 opam exec -- dune build --root vendor/drd/src/ml ocaml_opam_repo_trim.bc
 DUNE_BUILDDIR=vendor/drd/src/ml/_build/default
 
