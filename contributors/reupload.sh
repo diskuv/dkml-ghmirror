54677c88203fa91a670ce015abbf5807d80a9e4d
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-19 16:07:36 -0800

    Sync reupload script with release script

--- contributors/reupload.sh
+++ contributors/reupload.sh
@@ -12,23 +12,17 @@ DKMLDIR=$(cd "$DKMLDIR/.." && pwd)
 
 usage() {
     echo "Usage:" >&2
-    echo "    release.sh -h  Display this help message." >&2
-    echo "    release.sh -p  Create a prerelease." >&2
-    echo "    release.sh     Create a release." >&2
-    printf "Options:\n" >&2
-    printf "  -q: Quick mode. Creates a 'new' version of the OCaml Opam Repository that is a copy of the last version.\n" >&2
-    printf "      After GitLab CI rebuilds the repository, the new version will be updated with the rebuilt contents.\n" >&2
-    printf "      Without the quick mode, only one revision of the new version (the rebuilt contents) will be made.\n" >&2
+    echo "    reupload.sh -h  Display this help message." >&2
+    echo "    reupload.sh -p  Create a prerelease." >&2
+    echo "    reupload.sh     Create a release." >&2
 }
 
-QUICK=OFF
-while getopts ":hq" opt; do
+while getopts ":h" opt; do
     case ${opt} in
         h )
             usage
             exit 0
         ;;
-        q ) QUICK=ON ;;
         \? )
             echo "This is not an option: -$OPTARG" >&2
             usage
@@ -56,9 +50,9 @@ if which pacman >/dev/null 2>&1 && which cygpath >/dev/null 2>&1; then HOME="$US
 
 cd "$DKMLDIR"
 
-# Capture which version will be the release version when the prereleases are finished
-CURRENT_VERSION=0.4.0-prerel22
-OUT_VERSION=0.4.0-prerel26
+# Capture the version
+
+NEW_VERSION=$(awk '$1=="current_version"{print $NF; exit 0}' .bumpversion.prerelease.cfg)
 
 # ------------------------
 # GitLab Release
@@ -71,23 +65,15 @@ CI_PROJECT_ID='diskuv-ocaml%2Fdistributions%2Fdkml' # Must be url-encoded per ht
 
 # Setup Generic Packages (https://docs.gitlab.com/ee/user/packages/generic_packages/)
 PACKAGE_REGISTRY_GENERIC_URL="$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/generic"
-OOREPO_NEWURL="$PACKAGE_REGISTRY_GENERIC_URL/ocaml_opam_repo-reproducible/$OUT_VERSION"
-OOREPO_OLDURL="$PACKAGE_REGISTRY_GENERIC_URL/ocaml_opam_repo-reproducible/$CURRENT_VERSION"
-#OCAMLVERS=(4.14.0 4.13.1 4.12.1)
-OCAMLVERS=(4.14.0 4.12.1)
-#GITLAB_TARGET_VERSION=$(printf "%s" "$TARGET_VERSION" | tr +- ..) # replace -prerelM and +commitN with .prerelM and .commitN
+SUPPORT_NEWURL="$PACKAGE_REGISTRY_GENERIC_URL/ocaml_opam_repo-support/$NEW_VERSION"
 
-# Re-upload files from Generic Packages
+# Build ocaml_opam_repo_trim.bc (after the vendor/drd/ is cleaned, and just before upload)
+opam exec -- dune clean --root vendor/drd/src/ml
+opam exec -- dune build --root vendor/drd/src/ml ocaml_opam_repo_trim.bc
+DUNE_BUILDDIR=vendor/drd/src/ml/_build/default
 
-if [ "$QUICK" = ON ]; then
-    for OCAMLVER in "${OCAMLVERS[@]}"; do
-        for OOREPO_BASENAME in "ocaml-opam-repo-$OCAMLVER.tar.gz" "ocaml-opam-repo-$OCAMLVER.zip"; do
-            # download old
-            curl -L -o "contributors/_build/$OOREPO_BASENAME" "$OOREPO_OLDURL/$OOREPO_BASENAME"
-            # upload new
-            curl --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" \
-                --upload-file "contributors/_build/$OOREPO_BASENAME" \
-                "$OOREPO_NEWURL/$OOREPO_BASENAME"
-        done
-    done
-fi
+# Upload support files to Generic Packages
+
+curl --header "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" \
+     --upload-file "$DUNE_BUILDDIR/ocaml_opam_repo_trim.bc" \
+     "$SUPPORT_NEWURL/ocaml_opam_repo_trim.bc"
