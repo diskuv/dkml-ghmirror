2a545615bac33c80a071fed5b2845b9ccff899a9
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-27 11:32:43 -0800

    Update dependencies to 1.1.0

--- contributors/conf.py
+++ contributors/conf.py
@@ -21,7 +21,7 @@ import subprocess
 
 # -- Project information -----------------------------------------------------
 
-project = 'Diskuv OCaml 1.1.0-prerel27'
+project = 'Diskuv OCaml 1.1.0'
 copyright = '2021, Diskuv, Inc.'
 author = 'Diskuv, Inc.'
 
