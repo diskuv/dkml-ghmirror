54084b0b05a2629ff09509040b98a860d89f4d14
Jonah Beckford <71855677+jonahbeckford@users.noreply.github.com>
2022-12-26 22:10:30 -0800

    Update DKML_VERSION in setup-dkml.sh

--- contributors/release.sh
+++ contributors/release.sh
@@ -527,7 +527,8 @@ rungit push origin "$NEW_VERSION"
 
 # Update and push dkml-workflows-prerelease
 DOR=$(rungit -C "vendor/diskuv-opam-repository" rev-parse HEAD)
-sed_replace "s/DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=[a-f0-9]*$/DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=$DOR/" "$SRC_MIXED/dkml-workflows-prerelease/src/scripts/setup-dkml.sh"
+sed_replace "s/^DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=[a-f0-9]*$/DEFAULT_DISKUV_OPAM_REPOSITORY_TAG=$DOR/" "$SRC_MIXED/dkml-workflows-prerelease/src/scripts/setup-dkml.sh"
+sed_replace "s/^DKML_VERSION=[a-z0-9.-]*$/DKML_VERSION=$NEW_VERSION/" "$SRC_MIXED/dkml-workflows-prerelease/src/scripts/setup-dkml.sh"
 sed_replace 's#"PIN_DKML_APPS", *"[^"]*"#"PIN_DKML_APPS", "'"$OPAM_NEW_VERSION"'"#' "$SRC_MIXED/dkml-workflows-prerelease/src/logic/model.ml"
 sed_replace 's#"PIN_WITH_DKML", *"[^"]*"#"PIN_WITH_DKML", "'"$OPAM_NEW_VERSION"'"#' "$SRC_MIXED/dkml-workflows-prerelease/src/logic/model.ml"
 rungit -C "$SRC_MIXED/dkml-workflows-prerelease" add src/scripts/setup-dkml.sh src/logic/model.ml
