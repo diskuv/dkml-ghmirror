a5801d378c684814d7375a391d89b2fd17b1e018
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-06-24 21:16:17 -0700

    Bump version: 1.2.1-6 -> 1.2.1-7

--- version.cmake
+++ version.cmake
@@ -17,7 +17,7 @@ if(CMAKE_SCRIPT_MODE_FILE AND DKML_VERSION_CMAKEVER_OVERRIDE)
     set(DKML_VERSION_CMAKEVER "${DKML_VERSION_CMAKEVER_OVERRIDE}")
 else()
     # Edited by packaging/version-bump/CMakeLists.txt. Do not change format.
-    set(DKML_VERSION_CMAKEVER "1.2.1.6")
+    set(DKML_VERSION_CMAKEVER "1.2.1.7")
 endif()
 
 # The last released version (never a prerelease)
