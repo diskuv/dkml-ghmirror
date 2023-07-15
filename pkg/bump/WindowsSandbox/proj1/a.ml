d2149ccedff10ed292a1640f255cd50ca60c9c4b
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-07-15 12:01:17 -0700

    Fill in more Sandbox test scripts

---
+++ pkg/bump/WindowsSandbox/proj1/a.ml
@@ -0,0 +1,2 @@
+Logs.set_reporter (Logs.format_reporter ());
+Logs.err (fun m -> m "NO CARRIER");
--- pkg/bump/WindowsSandbox/test.in.cmd
+++ pkg/bump/WindowsSandbox/test.in.cmd
@@ -1,9 +1,13 @@
+for %%i in ("%~dp0.") do SET "sandbox=%%~fi"
+
 ocamlfind printconf
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dune build --root proj1
+dune build --root %sandbox%\proj1
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-utop script1/script.ocamlinit
+utop %sandbox%\script1\script.ocamlinit
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Once ocaml has a shim:
 REM - ocaml script1/script.ocamlinit
