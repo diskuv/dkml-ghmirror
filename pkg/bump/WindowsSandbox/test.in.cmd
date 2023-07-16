1676519ebdf05bbe84344f0ee4febf212cace2bd
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-07-15 22:57:38 -0700

    Expand Sandbox test to match CI

--- pkg/bump/WindowsSandbox/test.in.cmd
+++ pkg/bump/WindowsSandbox/test.in.cmd
@@ -3,7 +3,7 @@ for %%i in ("%~dp0.") do SET "sandbox=%%~fi"
 ocamlfind printconf
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dune build --root %sandbox%\proj1
+dune runtest --root %sandbox%\proj1
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 utop %sandbox%\script1\script.ocamlinit
@@ -11,3 +11,29 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Once ocaml has a shim:
 REM - ocaml script1/script.ocamlinit
+
+if not exist "%TEMP%\scratch" mkdir %TEMP%\scratch
+pushd %TEMP%\scratch
+
+dkml init --yes
+
+REM install something with a low number of dependencies, that sufficiently exercises Opam
+opam install graphics --yes
+if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+REM regression test: https://discuss.ocaml.org/t/ann-diskuv-ocaml-1-x-x-windows-ocaml-installer-no-longer-in-preview/10309/8?u=jbeckford
+opam install ppx_jane --yes
+if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+REM regression test: https://github.com/diskuv/dkml-installer-ocaml/issues/12
+opam install pyml --yes
+if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+REM regression test: https://github.com/diskuv/dkml-installer-ocaml/issues/21
+opam install ocaml-lsp-server merlin --yes
+if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+opam install ocamlformat --yes
+if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+popd
