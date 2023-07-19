2c822f486c70f6686fa40bb010044b8df07d6f0e
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-07-18 20:10:48 -0700

    Describe and warn about Generic Package uploads

--- pkg/bump/upload.in.cmake
+++ pkg/bump/upload.in.cmake
@@ -1,4 +1,4 @@
-message(NOTICE "Uploading Generic Package @UPLOAD_DESTFILE@ to GitLab Package Registry")
+message(NOTICE "Uploading Generic Package @UPLOAD_DESTFILE@ @UPLOAD_VERSION@ to GitLab Package Registry at @GITLAB_UPLOAD_BASE_URL@. WARNING: If the release needs to be recreated, the Generic Package must be deleted and then re-published or else the GitLab may (randomly) use the old Generic Package during downloads")
 file(UPLOAD @UPLOAD_SRCFILE@
     @GITLAB_UPLOAD_BASE_URL@/packages/generic/release/@UPLOAD_VERSION@/@UPLOAD_DESTFILE@
     HTTPHEADER "Authorization: Bearer @UPLOAD_TOKEN@"
