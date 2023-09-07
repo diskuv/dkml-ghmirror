2272aab6f16de2cf7c89a4c8de655499b4bb0f51
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-09-06 22:33:53 -0700

    Add ?select=package_file during UPLOAD

--- pkg/bump/upload.in.cmake
+++ pkg/bump/upload.in.cmake
@@ -1,5 +1,5 @@
 message(NOTICE "Uploading Generic Package @UPLOAD_DESTFILE@ @UPLOAD_VERSION@ to GitLab Package Registry at @GITLAB_UPLOAD_BASE_URL@. WARNING: If the release needs to be recreated, the Generic Package must be deleted and then re-published or else the GitLab may (randomly) use the old Generic Package during downloads")
 file(UPLOAD @UPLOAD_SRCFILE@
-    @GITLAB_UPLOAD_BASE_URL@/packages/generic/release/@UPLOAD_VERSION@/@UPLOAD_DESTFILE@
+    "@GITLAB_UPLOAD_BASE_URL@/packages/generic/release/@UPLOAD_VERSION@/@UPLOAD_DESTFILE@?select=package_file"
     HTTPHEADER "Authorization: Bearer @UPLOAD_TOKEN@"
 )
