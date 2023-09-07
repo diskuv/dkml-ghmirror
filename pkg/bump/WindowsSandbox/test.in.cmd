e33e451432042b937582ae430f05d496d896e426
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-09-06 22:49:18 -0700

    Run more native-only tests when native mode

--- pkg/bump/WindowsSandbox/test.in.cmd
+++ pkg/bump/WindowsSandbox/test.in.cmd
@@ -15,32 +15,8 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 REM Once ocaml has a shim:
 REM - ocaml script1/script.ocamlinit
 
-if not exist "%TEMP%\scratch2" mkdir %TEMP%\scratch2
-pushd %TEMP%\scratch2
-
-dkml init --yes
-
-REM install something with a low number of dependencies, that sufficiently exercises Opam
-opam install graphics --yes
-if %errorlevel% neq 0 popd & exit /b %errorlevel%
-
-REM regression test: https://discuss.ocaml.org/t/ann-diskuv-ocaml-1-x-x-windows-ocaml-installer-no-longer-in-preview/10309/8?u=jbeckford
-opam install ppx_jane --yes
-if %errorlevel% neq 0 popd & exit /b %errorlevel%
-
-REM regression test: https://github.com/diskuv/dkml-installer-ocaml/issues/12
-opam install pyml --yes
-if %errorlevel% neq 0 popd & exit /b %errorlevel%
-
-REM regression test: https://github.com/diskuv/dkml-installer-ocaml/issues/21
-opam install ocaml-lsp-server merlin --yes
-if %errorlevel% neq 0 popd & exit /b %errorlevel%
-
-opam install ocamlformat --yes
-if %errorlevel% neq 0 popd & exit /b %errorlevel%
-
 CALL %LOCALAPPDATA%\Programs\DiskuvOCaml\dkmlvars.cmd
-if %DiskuvOCamlMode% == "byte" (
+if "%DiskuvOCamlMode%" == "byte" (
     REM Dune as of 3.8.3 requires explicit xxx.bc on the command line or else
     REM it will do -output-complete-exe which requires a C linker
     dune build --root %TEMP%\scratch1\proj1 ./a.bc
@@ -48,11 +24,35 @@ if %DiskuvOCamlMode% == "byte" (
     ocamlrun %TEMP%\scratch1\proj1\_build\default\a.bc
     if %errorlevel% neq 0 exit /b %errorlevel%
 )
-if %DiskuvOCamlMode% == "native" (
+if "%DiskuvOCamlMode%" == "native" (
+    if not exist "%TEMP%\scratch2" mkdir %TEMP%\scratch2
+    pushd %TEMP%\scratch2
+
+    dkml init --yes
+
+    REM install something with a low number of dependencies, that sufficiently exercises Opam
+    opam install graphics --yes
+    if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+    REM regression test: https://discuss.ocaml.org/t/ann-diskuv-ocaml-1-x-x-windows-ocaml-installer-no-longer-in-preview/10309/8?u=jbeckford
+    opam install ppx_jane --yes
+    if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+    REM regression test: https://github.com/diskuv/dkml-installer-ocaml/issues/12
+    opam install pyml --yes
+    if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+    REM regression test: https://github.com/diskuv/dkml-installer-ocaml/issues/21
+    opam install ocaml-lsp-server merlin --yes
+    if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
+    opam install ocamlformat --yes
+    if %errorlevel% neq 0 popd & exit /b %errorlevel%
+
     dune build --root %TEMP%\scratch1\proj2
     if %errorlevel% neq 0 exit /b %errorlevel%
     dune exec --root %TEMP%\scratch1\proj2 ./best.exe
     if %errorlevel% neq 0 exit /b %errorlevel%
-)
 
-popd
+    popd
+)
