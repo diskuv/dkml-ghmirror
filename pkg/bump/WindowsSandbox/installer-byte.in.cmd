2bb506d84e051eace0d89bcdfb32ee588a9ef836
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-11-22 03:27:41 -0800

    WindowsSandbox tests run in any dir

--- pkg/bump/WindowsSandbox/installer-byte.in.cmd
+++ pkg/bump/WindowsSandbox/installer-byte.in.cmd
@@ -1,3 +1,3 @@
 time /t
-Switch\share\dkml-installer-ocaml-offline\t\unsigned-dkml-byte-windows_x86_64-i-@DKML_VERSION_SEMVER@.exe
+%~dp0Switch\share\dkml-installer-ocaml-offline\t\unsigned-dkml-byte-windows_x86_64-i-@DKML_VERSION_SEMVER@.exe
 time /t
--- pkg/bump/WindowsSandbox/installer-native.in.cmd
+++ pkg/bump/WindowsSandbox/installer-native.in.cmd
@@ -1,3 +1,3 @@
 time /t
-Switch\share\dkml-installer-ocaml-network\t\unsigned-dkml-native-windows_x86_64-i-@DKML_VERSION_SEMVER@.exe
+%~dp0..\Switch\share\dkml-installer-ocaml-network\t\unsigned-dkml-native-windows_x86_64-i-@DKML_VERSION_SEMVER@.exe
 time /t
